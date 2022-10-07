bf5d6c566150b88e2cf2b97fba75ffe3aecf2e34
CS-5 <me@carsonseese.com>
2022-10-07 13:08:15 -0400

    Remove flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: ["class", "[data-theme='dark']"],
   corePlugins: { preflight: false },
-  plugins: [require("flowbite/plugin"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography")],
   theme: {
     extend: {
       fontFamily: {
