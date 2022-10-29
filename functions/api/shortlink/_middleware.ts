c36086d59b85b01136852e9c869813485e881458
CS-5 <me@carsonseese.com>
2022-10-29 18:06:55 -0400

    Bug comment

--- functions/api/shortlink/_middleware.ts
+++ functions/api/shortlink/_middleware.ts
@@ -2,6 +2,10 @@ import cloudflareAccessPlugin, {
   PluginData,
 } from "@cloudflare/pages-plugin-cloudflare-access";
 
+// BUG: This is currently broken, sending the user into a redirect loop.
+// See https://discord.com/channels/595317990191398933/967071916160610344/1023227497497759784
+// and https://github.com/cloudflare/pages-plugins/issues/13 for more info
+
 /* eslint-disable */
 export const onRequestGet: PagesFunction<
   { AUTH_AUD: string; AUTH_DISABLED: string },
