c8941d609dd76e655aeda08b08cc0cbd6b5590e9
CS-5 <me@carsonseese.com>
2023-01-01 15:48:24 -0500

    Biohazard homepage - coming soon

--- src/pages/biohazard/index.tsx
+++ src/pages/biohazard/index.tsx
@@ -1,23 +1,9 @@
 import DefaultLayout from "@site/src/layouts/Default";
 import React from "react";
-import HeroHeader from "../../components/home/hero/Header";
-import HomeSection from "../../components/home/Section";
 
 export default function Biohazard(): JSX.Element {
   return (
     <DefaultLayout>
-      <HeroHeader className="bg-[url('/img/people-cheering.webp')] bg-cover bg-bottom text-white">
-        <h1 className="mx-auto h-28 w-4/5 text-center text-4xl drop-shadow-md md:w-1/2 md:text-5xl">
-          Biohazard
-        </h1>
-        <div className="mt-10 flex justify-center space-x-2">
-          {/*}
-            <Button href="/about" value="About Us" />
-            <Button href="/sponsors" value="Our Sponsors" />
-            <Button href="/blog" value="Our Blog" />
-          {*/}
-        </div>
-      </HeroHeader>
       <main className="space-y-32 border-0 border-t-2 border-solid border-green border-opacity-20 p-48 dark:border-opacity-10">
         Coming Soon!
       </main>
