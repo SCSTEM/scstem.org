a7258c40ca38d5c0d69d0d05e738a626c90544df
CS-5 <me@carsonseese.com>
2023-01-02 10:54:44 -0500

    Minor get-involved form size adjustment

--- src/pages/get-involved.tsx
+++ src/pages/get-involved.tsx
@@ -5,7 +5,7 @@ export default function GetInvolved(): JSX.Element {
   return (
     <DefaultLayout>
       <iframe
-        className="h-[800px] w-full bg-[url('/img/svg/circuit-board-light.svg')] dark:bg-[url('/img/svg/circuit-board-dark.svg')] md:h-[900px]"
+        className="h-[800px] w-full bg-[url('/img/svg/circuit-board-light.svg')] p-8 dark:bg-[url('/img/svg/circuit-board-dark.svg')] md:h-[1200px]"
         src="https://docs.google.com/forms/d/e/1FAIpQLScTjT3LHFAq1mOfKFztgMOpUT8hFWz81dYlaaDa4B8lG6yr2Q/viewform?embedded=true"
       >
         Loading…
