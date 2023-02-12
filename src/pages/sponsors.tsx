0215ca1c0018758ce3265bf4349af1e4d296ca44
Carson Seese <carson@carsonseese.com>
2023-02-12 09:38:38 -0500

    Fix overlay and sizing

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import DefaultLayout from "@site/src/layouts/Default";
 import SponsorCard from "@site/src/components/SponsorCard";
 import { Sponsors as data } from "@site/data";
-import { Overlay, Title, Text, Container } from "@mantine/core";
+import { Overlay, Container } from "@mantine/core";
 
 export default function Sponsors(): JSX.Element {
   return (
     <DefaultLayout>
-      <div className="bg-[url('/img/legos.webp')] bg-cover bg-center">
+      <div className="relative h-screen bg-[url('/img/legos.webp')] bg-cover bg-center md:h-[500px]">
         <Overlay
-          gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.65) 40%)"
-          opacity={0.8}
+          gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.7) 40%)"
+          opacity={1}
           zIndex={0}
         />
-        <Container className="relative z-10 flex h-[400px] flex-col items-start justify-end space-y-4 pb-28 text-white">
+        <Container className="relative z-10 flex flex-col items-start justify-end space-y-6 pt-20 text-white md:h-full md:pb-36">
           <div className="text-5xl font-bold">
             Sponsors help us{" "}
             <span className="bg-gradient-to-r from-m_yellow-5 to-m_yellow-9 bg-clip-text text-transparent dark:from-m_yellow-4 dark:to-m_yellow-7">
@@ -28,7 +28,7 @@ export default function Sponsors(): JSX.Element {
           </div>
         </Container>
       </div>
-      <div className="grid grid-cols-1 gap-10 p-28 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="mx-auto grid grid-cols-1 gap-10 py-6 md:grid-cols-2 md:py-20 lg:grid-cols-3 xl:grid-cols-4">
         {data.map((sponsor) => (
           <SponsorCard key={sponsor.name} {...sponsor} />
         ))}
