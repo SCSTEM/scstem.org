9e6ba4e491f754969e5f94be766908e59b6bce38
Carson Seese <carson@carsonseese.com>
2023-02-14 08:08:23 -0500

    Better scroll-down

--- src/components/HeroHeader.tsx
+++ src/components/HeroHeader.tsx
@@ -33,23 +33,28 @@ export default function HeroHeader({
         <div
           className={clsx(
             fullscreen ? "md:mt-60 md:h-screen" : "h-full md:mt-48",
-            "z-10 mx-8 mt-10 flex-grow md:m-0 md:ml-36 md:w-9/12 lg:ml-60 lg:w-1/2"
+            "z-10 mx-8 mt-20 flex-grow md:m-0 md:ml-36 md:w-9/12 lg:ml-60 lg:w-1/2"
           )}
         >
           {children}
         </div>
         <button
-          id="scrollButton"
-          className="mx-auto mb-20 flex cursor-pointer flex-col items-center border-none bg-transparent p-0 text-lg font-medium outline-none dark:text-primary md:hidden"
+          className="mx-auto mb-10 flex cursor-pointer flex-col items-center border-none bg-transparent p-0 text-lg font-medium outline-none dark:text-primary md:hidden"
           onClick={() =>
             document
-              .getElementById("scrollButton")
+              .getElementById("scrollhere")
               .scrollIntoView({ behavior: "smooth", block: "start" })
           }
         >
           Find out more
-          <IconChevronDown size={50} stroke={1.5} className="animate-bounce" />
+          <IconChevronDown
+            size={50}
+            stroke={1.5}
+            className="animate-bounce mt-1"
+          />
         </button>
+        {/* This is a hack to scroll to the top of the body */}
+        <div id="scrollhere" className="mb-10"></div>
       </div>
     </header>
   );
