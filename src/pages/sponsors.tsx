65327fada03fce90eb6cdbaaee55c85a6ec5f857
Carson Seese <carson@carsonseese.com>
2023-02-14 08:08:14 -0500

    Update imports

--- src/pages/sponsors.tsx
+++ src/pages/sponsors.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 
 import { Sponsors as data } from "@site/data";
-import HeroHeader2 from "@site/src/components/HeroHeader";
+import HeroHeader from "@site/src/components/HeroHeader";
+import Highlight from "@site/src/components/Highlight";
 import SponsorCard from "@site/src/components/SponsorCard";
 import DefaultLayout from "@site/src/layouts/Default";
 
 export default function Sponsors(): JSX.Element {
   return (
     <DefaultLayout>
-      <HeroHeader2 img="/img/legos.webp">
+      <HeroHeader img="/img/legos.webp">
         <div className="flex flex-col space-y-6 text-white">
           <div className="text-5xl font-bold">
-            Sponsors help us{" "}
-            <span className="bg-gradient-to-r from-m_yellow-5 to-m_yellow-9 bg-clip-text text-transparent dark:from-m_yellow-4 dark:to-m_yellow-7">
-              build
-            </span>
-            .
+            Sponsors help us <Highlight theme="dark">build</Highlight>.
           </div>
           <div className="text-2xl">
             We are proud to partner organizations and individuals who want to
@@ -23,7 +20,7 @@ export default function Sponsors(): JSX.Element {
             mentors, volunteers, and parents to focus on what really matters.
           </div>
         </div>
-      </HeroHeader2>
+      </HeroHeader>
       <div className="mx-auto grid grid-cols-1 gap-10 py-6 md:grid-cols-2 md:py-20 lg:grid-cols-3 xl:grid-cols-4">
         {data.map((sponsor) => (
           <SponsorCard key={sponsor.name} {...sponsor} />
