a508e2dfced712355671254f7cfeb897968d4cc2
Carson Seese <carson@carsonseese.com>
2023-03-04 07:33:01 -0500

    Remove GTM

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,9 +92,6 @@ const config = {
         gtag: {
           trackingID: "G-3TPD3DLYBR",
         },
-        googleTagManager: {
-          containerId: "GTM-TBS6KW7",
-        },
         sitemap: {
           changefreq: "monthly",
           priority: 0.5,
