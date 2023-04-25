7c8f8817eb54f706f55604c9ddfb7eb1f28be669
Carson Seese <carson@carsonseese.com>
2023-04-24 21:05:23 -0400

    Fix workspace config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
   root: true,
-  plugins: ["@docusaurus", "@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:@docusaurus/recommended",
-    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   env: {
     node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/no-unescaped-entities": "off",
+    "react/prop-types": "off",
   },
 };
