1686c8e20f0a7cb03566a5d5c04b91cf7c627966
Carson Seese <carson@carsonseese.com>
2023-04-30 17:38:12 -0400

    Tweak Get Involved form

--- src/pages/get-involved.tsx
+++ src/pages/get-involved.tsx
@@ -2,13 +2,18 @@ import DefaultLayout from "@site/src/layouts/Default";
 
 export default function GetInvolved(): JSX.Element {
   return (
-    <DefaultLayout>
-      <iframe
-        className="h-[800px] w-full bg-[url('/img/svg/circuit-board-light.svg')] p-8 dark:bg-[url('/img/svg/circuit-board-dark.svg')] md:h-[1200px]"
-        src="https://docs.google.com/forms/d/e/1FAIpQLScTjT3LHFAq1mOfKFztgMOpUT8hFWz81dYlaaDa4B8lG6yr2Q/viewform?embedded=true"
-      >
-        Loading…
-      </iframe>
+    <DefaultLayout
+      title="Get Involved"
+      description="Interested in one of our programs? Fill out this brief form to Get Involved"
+    >
+      <div className="bg-[url('/img/svg/circuit-board-light.svg')] dark:bg-[url('/img/svg/circuit-board-dark.svg')] h-[1800px] w-full">
+        <iframe
+          className="px-4 py-8 md:p-8 h-full w-full"
+          src="https://docs.google.com/forms/d/e/1FAIpQLScTjT3LHFAq1mOfKFztgMOpUT8hFWz81dYlaaDa4B8lG6yr2Q/viewform?embedded=true"
+        >
+          Loading…
+        </iframe>
+      </div>
     </DefaultLayout>
   );
 }
