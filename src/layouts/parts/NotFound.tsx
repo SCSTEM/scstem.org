1ad77f80d870ecc524c22af7d96a028e8c9c086f
Carson Seese <carson@carsonseese.com>
2023-04-30 18:56:56 -0400

    Temp 404 page

---
+++ src/layouts/parts/NotFound.tsx
@@ -0,0 +1,14 @@
+import Link from "@docusaurus/Link";
+import IdealImage from "@theme/IdealImage";
+
+export default function NotFound(): JSX.Element {
+  return (
+    <div className="text-center my-24 flex flex-col space-y-10 md:max-w-xl max-w-xs mx-auto">
+      <div>
+        Your page could not be found, and neither could the design for our 404
+        page. Click <Link to="/">here</Link> to go home.
+      </div>
+      <IdealImage img={require("../../idealimage/sad-cube.png")} />
+    </div>
+  );
+}
