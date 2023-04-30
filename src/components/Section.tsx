f7acb75e2be7d22162e3bfc83a6ebd5150b24c0d
Carson Seese <carson@carsonseese.com>
2023-04-30 17:51:59 -0400

    Homepage cleanup

---
+++ src/components/Section.tsx
@@ -0,0 +1,35 @@
+import { Title } from "@mantine/core";
+
+type Props = {
+  children: React.ReactNode;
+} & (
+  | {
+      alt?: false;
+      title: string;
+    }
+  | {
+      alt?: true;
+      title?: never;
+    }
+);
+
+export default function Section({ children, alt, title }: Props): JSX.Element {
+  if (alt)
+    return (
+      <div className="space-y-4 bg-zinc-200 dark:bg-black shadow-inner p-8">
+        <div className="md:max-w-screen-xl mx-auto md:px-12">{children}</div>
+      </div>
+    );
+
+  return (
+    <div className="p-8 md:max-w-screen-xl mx-auto">
+      <Title
+        order={3}
+        className="text-2xl font-black mb-6 text-center md:text-left"
+      >
+        {title}
+      </Title>
+      {children}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,7 @@ import FeatureCard, {
 import { Button } from "@site/src/components/inputs/Button";
 import DefaultLayout from "@site/src/layouts/Default";
 
+import Section from "../components/Section";
 import Underline from "../components/spans/Underline";
 
 const programs: FeatureCardProps[] = [
@@ -236,7 +237,7 @@ export default function Home(): JSX.Element {
         </Section>
 
         {/* FIRST */}
-        <AltSection>
+        <Section alt>
           <div className="md:w-[750px] mb-8 mx-auto">
             <IdealImage
               img={require("../idealimage/first/first-horizontal-acro-light.png")}
@@ -320,7 +321,7 @@ export default function Home(): JSX.Element {
               className="overflow-hidden rounded-3xl md:max-w-2xl"
             />
           </div>
-        </AltSection>
+        </Section>
 
         {/* Why Join */}
         <Section title="Why join?">
@@ -378,114 +379,7 @@ export default function Home(): JSX.Element {
             <Link to="/get-involved">Get Involved form</Link>.
           </Text>
         </div>
-
-        {/*
-        <HomeSection
-          title={`What is the "South Central STEM Collective"?`}
-          image="/img/svg/undraw/building.svg"
-        >
-          <p>
-            The South Central STEM Collective (otherwise known as SC2) was
-            created to serve South Central Pennsylvania as "
-            <Highlight>STEM Central</Highlight>" to inspire youth aged K-12
-            through hands-on education, competitive robotics and drone teams,
-            maker spaces, and community outreach.
-          </p>
-          <p>
-            To achieve this goal, the SC2 plans to offer a range of activities,
-            resources, space, and industry expertise in addition to the
-            following:
-          </p>
-
-          <ul>
-            <li>
-              Acquire and maintain a collaborative workspace for communal use by
-              all groups and members.
-            </li>
-            <li>
-              Foster a creative and collaborative environment for
-              experimentation and development in technology.
-            </li>
-            <li>
-              Provide STEM education and mentorship to youth in grades K-12 as
-              it pertains to robotics, drones, and maker labs.
-            </li>
-            <li>
-              Share technology developments and ideas with the world at large.
-            </li>
-          </ul>
-
-          <div className="flex justify-center"></div>
-        </HomeSection>
-
-        <HomeSection
-          title="The only thing missing is you!"
-          image="/img/svg/undraw/high-five.svg"
-          flipped
-        >
-          <p>
-            Are you a <Highlight>student</Highlight> who wants to grow your
-            skills in Science, Technology, Engineering, or Math? Do you have an
-            interest in marketing, business, or leadership? Do you like
-            photography or design?
-          </p>
-          <p>
-            Are you an industry pro, educator, business person, or engineer? Do
-            you have a special skill-set,{" "}
-            <Highlight>enjoy working with students</Highlight>, or have a
-            passion for non-profit management? Are you looking for an outlet
-            where you can give back to the community or{" "}
-            <Highlight>share your skills</Highlight> and experience?
-          </p>
-          <p>
-            Do you <Highlight>own a business</Highlight>, have available funds
-            you are looking to invest back into the community, or are looking
-            for a local non-profit to support? Are you looking for a place to
-            donate tools, equipment, computers, or other materials?
-          </p>
-          <p>
-            If you answered <strong>yes</strong> to any of these questions, we
-            definitely have a place for you. Click the button below to get
-            plugged in, or feel free to{" "}
-            <a href="/contact">reach out directly</a> if you have any specific
-            questions.
-          </p>
-          <div className="flex justify-center">
-            <BrandButton to="/get-involved" className="text-dark">
-              Get Involved
-            </BrandButton>
-          </div>
-        </HomeSection>
-        */}
       </div>
     </DefaultLayout>
   );
 }
-
-const AltSection = ({
-  children,
-}: {
-  children: React.ReactNode;
-}): JSX.Element => (
-  <div className="space-y-4 bg-zinc-200 dark:bg-black shadow-inner p-8">
-    <div className="md:max-w-screen-xl mx-auto md:px-12">{children}</div>
-  </div>
-);
-
-const Section = ({
-  title,
-  children,
-}: {
-  title: string;
-  children: React.ReactNode;
-}): JSX.Element => (
-  <div className="p-8 md:max-w-screen-xl mx-auto">
-    <Title
-      order={3}
-      className="text-2xl font-black mb-6 text-center md:text-left"
-    >
-      {title}
-    </Title>
-    {children}
-  </div>
-);
