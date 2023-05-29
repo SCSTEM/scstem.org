1ad32efaf454850504719f6212a96d1792a2996b
Carson Seese <carson@carsonseese.com>
2023-05-29 14:41:42 -0400

    WIP robot stats

--- src/pages/biohazard/index.tsx
+++ src/pages/biohazard/index.tsx
@@ -1,17 +1,25 @@
 import {
+  Accordion,
   Card,
   Col,
   Grid,
+  MantineColor,
   Overlay,
   SimpleGrid,
   ThemeIcon,
   Title,
   clsx,
+  useMantineColorScheme,
+  useMantineTheme,
 } from "@mantine/core";
 import { Text, rem } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import {
+  IconAward,
   IconChevronDown,
+  IconDeviceGamepad2,
+  IconEngine,
+  IconHandGrab,
   IconMoodHappy,
   IconPlayerPlayFilled,
   IconRobot,
@@ -27,11 +35,12 @@ import Scroller from "@site/src/components/ImageScroller";
 import { BrandButton } from "@site/src/components/inputs/Button";
 import Underline from "@site/src/components/spans/Underline";
 import DefaultLayout from "@site/src/layouts/Default";
+import { breakpoints } from "@site/src/styles/styles";
 
 import "./index.css";
 
 export default function BiohazardHome(): JSX.Element {
-  const isSmall = useMediaQuery("(max-width: 800px)");
+  const isSmall = useMediaQuery(`(max-width: ${breakpoints.sm})`);
 
   return (
     <DefaultLayout
@@ -56,11 +65,11 @@ export default function BiohazardHome(): JSX.Element {
           </video>
         </div>
         <div className="relative flex h-full flex-col w-full lg:w-[750px] xl:w-[1000px] mx-auto text-center">
-          <img className="md:mt-28 my-2" src="/img/biohazard/header-logo.svg" />
-          <div className="mx-5 space-y-4 md:space-y-10 mb-4 md:p-8">
+          <img className="xl:mt-28 mt-2" src="/img/biohazard/header-logo.svg" />
+          <div className="mx-5 space-y-4 xl:space-y-10 mb-4 md:p-8">
             <Title
               order={1}
-              className="text-xl md:text-4xl lg:text-4xl lg:!leading-[3rem]"
+              className="text-xl md:text-3xl xl:text-4xl md:!leading-[3rem]"
             >
               Welcome to the{" "}
               <Underline color="brand-green">next generation</Underline> of
@@ -111,14 +120,12 @@ export default function BiohazardHome(): JSX.Element {
               {
                 speed: -15,
                 children: (
-                  <div className="absolute top-[150px] lg:top-[200px] xl:top-[250px] w-full">
-                    <div className="rounded-lg lg:ml-10 mx-4 shadow-xl text-white p-4 md:w-fit bg-gradient-to-br from-brand-green-3 via-brand-green-4 to-brand-green-7 space-y-2 text-center">
-                      <div className="font-sans font-black text-2xl">
-                        Biohazard's 2023 Robot:
-                      </div>
-                      <div className="font-heading text-5xl font-black italic">
-                        Viper
-                      </div>
+                  <div className="absolute top-[150px] lg:top-[200px] xl:top-[200px] w-full">
+                    <div className="rounded-lg lg:ml-10 mx-4 shadow-xl text-white p-4 w-[200px] md:max-w-sm md:w-fit from-brand-green-3 via-brand-green-4 to-brand-green-7 space-y-2 text-center">
+                      <img
+                        src="/img/biohazard/viper-logo.webp"
+                        className="aspect-square"
+                      />
                     </div>
                   </div>
                 ),
@@ -126,39 +133,7 @@ export default function BiohazardHome(): JSX.Element {
             ]}
           />
           <PageSection>
-            <div className="my-16 md:my-36 md:mx-10 relative h-[400px] md:h-full">
-              <div className="bg-gradient-to-br from-brand-gray-3 to-brand-gray-8 absolute h-full md:h-[550px] w-full -z-10 rounded-2xl shadow-2xl top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 border-slate-600 border-solid border-[1px]" />
-              <div className="flex md:flex-row flex-col mx-5">
-                <Card
-                  className="flex-1 -mt-10 mb-4 md:my-4 mx-8"
-                  shadow="xl"
-                  padding="lg"
-                  radius="lg"
-                  withBorder
-                >
-                  <div className="font-heading text-3xl lg:text-5xl font-black italic text-green">
-                    Viper
-                  </div>
-                </Card>
-                <div className="md:max-w-xl md:py-24 px-5">
-                  <Scroller
-                    delay={5000}
-                    images={[
-                      "/img/biohazard/renders/2023-robot-render-1.webp",
-                      "/img/biohazard/renders/2023-robot-render-2.webp",
-                      "/img/biohazard/renders/2023-robot-render-3.webp",
-                      "/img/biohazard/renders/2023-robot-render-4.webp",
-                    ].map((src, i) => (
-                      <img
-                        key={i}
-                        src={src}
-                        className="object-contain h-[200px] lg:h-full my-auto"
-                      />
-                    ))}
-                  />
-                </div>
-              </div>
-            </div>
+            <ViperProfile />
           </PageSection>
           <PageSection className="xl:!mt-10">
             <div className="aspect-video lg:m-20 rounded-md p-2 bg-gradient-to-br from-brand-green-3 to-brand-green-8 shadow-2xl">
@@ -179,6 +154,130 @@ export default function BiohazardHome(): JSX.Element {
         </div>
       </main>
     </DefaultLayout>
+  );
+}
+
+function ViperProfile() {
+  const { colorScheme } = useMantineColorScheme();
+  const { colors } = useMantineTheme();
+  const sm = useMediaQuery(`(max-width: ${breakpoints.sm})`);
+  const isLg = useMediaQuery(`(max-width: ${breakpoints.xl})`);
+
+  const features: {
+    title: string;
+    description: ReactNode;
+    icon: FC<TablerIconsProps>;
+    color?: MantineColor;
+  }[] = [
+    {
+      title: "Drivetrain",
+      description:
+        "Viper's drivetrain uses 4 mecanum wheels for rapid forward, backward, left, and right motion powered by 4 Vex Falcon 500 motors.",
+      icon: IconEngine,
+      color: "brand-blue",
+    },
+    {
+      title: "Manipulator",
+      description:
+        "The claw is made of 3D printed parts using pneumatic cylinders to open and close. The fingers are mounted on a 3D printed, rotating wrist.",
+      icon: IconHandGrab,
+      color: "brand-red",
+    },
+    {
+      title: "Controls",
+      description:
+        "Viper is operated by primary and secondary drivers using Xbox controllers. The primary controls the drivetrain while the secondary controls the manipulator.",
+      icon: IconDeviceGamepad2,
+      color: "grape",
+    },
+    {
+      title: "Winning",
+      description: (
+        <>
+          Biohazard and Viper won the 2023 Greater Pittsburgh Regional{" "}
+          <i>Industrial Design Award</i> due to their selection of material
+          types and complex assemblies.
+        </>
+      ),
+      icon: IconAward,
+      color: "brand-yellow",
+    },
+  ];
+
+  return (
+    <div className="my-10 xl:my-0 md:mx-10 mx-1 relative h-[500px] md:h-[750px] xl:h-full">
+      <div className="bg-gradient-to-br from-brand-gray-3 to-brand-gray-8 absolute h-full md:h-[850px] xl:h-[550px] w-full -z-10 rounded-2xl shadow-2xl top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 border-slate-600 border-solid border-[1px]" />
+      <div className="flex xl:flex-row flex-col mx-5">
+        <Card
+          className="flex-1 -mt-12 md:-mt-32 xl:mt-0 mb-4 md:my-4 -ml-10 mr-3 md:mx-8 md:p-8 md:pl-6 p-4 pl-2 h-fit"
+          shadow="xl"
+          radius="lg"
+          bg={colorScheme === "dark" ? undefined : colors["brand-green"][7]}
+          withBorder
+        >
+          <div className="font-heading text-3xl lg:text-5xl font-black italic dark:text-green text-white">
+            Viper
+          </div>
+          {isLg ? (
+            <Accordion className="mt-2" defaultValue={features[0].title}>
+              {features.map((feature, i) => (
+                <Accordion.Item
+                  key={i}
+                  value={feature.title}
+                  className="[&:is(:last-child)]:border-b-0"
+                >
+                  <Accordion.Control
+                    className="dark:text-green text-white"
+                    color={`#${colors[feature.color][5]}`}
+                  >
+                    <div className="flex items-center space-x-2">
+                      <feature.icon size={rem(36)} stroke={1.5} />
+                      <div className="w-full font-heading">{feature.title}</div>
+                    </div>
+                  </Accordion.Control>
+                  <Accordion.Panel className="text-brand-gray-0 text-sm">
+                    {feature.description}
+                  </Accordion.Panel>
+                </Accordion.Item>
+              ))}
+            </Accordion>
+          ) : (
+            <div className="mt-4 md:mt-10 ml-1 md:ml-2 grid xl:grid-cols-2 grid-cols-1 md:grid-cols-4 gap-y-6 md:gap-y-10 gap-x-10">
+              {features.map((feature, i) => (
+                <div key={i} className="space-y-2">
+                  <div className="flex dark:text-green text-white space-x-2 pr-4">
+                    <feature.icon size={sm ? rem(30) : rem(36)} stroke={1.5} />
+                    <div className="w-full border-b-2 border-solid border-0 md:text-xl font-heading">
+                      {feature.title}
+                    </div>
+                  </div>
+                  <div className="text-brand-gray-0 text-sm md:text-base">
+                    {feature.description}
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </Card>
+        <div className="md:max-w-xl xl:py-24 px-5 mx-auto">
+          <Scroller
+            delay={5000}
+            images={[
+              "/img/biohazard/renders/2023-robot-render-1.webp",
+              "/img/biohazard/renders/2023-robot-render-2.webp",
+              "/img/biohazard/renders/2023-robot-render-3.webp",
+              "/img/biohazard/renders/2023-robot-render-4.webp",
+            ].map((src, i) => (
+              <img
+                key={i}
+                src={src}
+                className="object-contain h-[200px] lg:h-full my-auto"
+              />
+            ))}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
