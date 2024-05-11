f1b7c6acbcc2987680ee3f110ae637aa7b1bd86e
Carson Seese <carson@carsonseese.com>
2024-05-10 21:07:15 -0400

    Update wiki links

--- data/config.ts
+++ data/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   donateUrl: "https://www.paypal.com/US/fundraiser/charity/4486755",
-  wishlistUrl: "https://example.com", // TODO
-  sponsorPacketUrl: "https://example.com", // TODO,
+  wishlistUrl: "https://wiki.scstem.org/donations/wishlist",
+  sponsorPacketUrl: "https://wiki.scstem.org/donations/packet",
   getInvolvedUrl:
     "https://docs.google.com/forms/d/e/1FAIpQLScTjT3LHFAq1mOfKFztgMOpUT8hFWz81dYlaaDa4B8lG6yr2Q/viewform?embedded=true",
 };
