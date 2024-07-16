bb67ed192ae034f691301ee45a849b7d983ca7e0
Kiefer Martin <agentaston967st@gmail.com>
2024-07-16 09:13:29 -0400

    final touches

--- src/app/programs/frc/robots/page.tsx
+++ src/app/programs/frc/robots/page.tsx
@@ -1,27 +1,37 @@
-import { Image } from "@/components/Image";
-import viperLogo from "@/image/biohazard/viper-logo.webp";
-import troubleClef from "@/image/biohazard/trouble-logo.webp";
-import { ParallaxImage } from "../components";
-import { PatternBackground } from "@/components/PatternBackground";
 import { Carousel } from "@/components/Carousel";
+import { Image } from "@/components/Image";
 import { ManualCarousel } from "@/components/ManualCarousel";
-import { CarouselNext, CarouselPrevious } from "@/components/shadcn/ui/carousel";
+import { PatternBackground } from "@/components/PatternBackground";
+import {
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/shadcn/ui/carousel";
+
+import troubleClef from "@/image/biohazard/trouble-logo.webp";
+import viperLogo from "@/image/biohazard/viper-logo.webp";
+
+import { ParallaxImage } from "../components";
 
 function SlideButtons() {
   return (
     <>
-      <CarouselPrevious className={"w-20 h-24 opacity-85"} variant={"default"} size={"lg"}>
-        
-      </CarouselPrevious>
-      <CarouselNext className={"w-20 h-24 opacity-85"} variant={"default"} size={"lg"}>
-    
-      </CarouselNext>
+      <CarouselPrevious
+        className={"w-20 h-24 opacity-85"}
+        variant={"default"}
+        size={"lg"}
+      ></CarouselPrevious>
+      <CarouselNext
+        className={"w-20 h-24 opacity-85"}
+        variant={"default"}
+        size={"lg"}
+      ></CarouselNext>
     </>
-  )
+  );
 }
 
-const slides = [(
+const slides = [
   <>
+    <h1 className="heading-2 text-7xl text-center">2019</h1>
     <ParallaxImage background="/image/biohazard/2019-robot-field.webp">
       <SlideButtons></SlideButtons>
     </ParallaxImage>
@@ -30,15 +40,17 @@ const slides = [(
         <div className="py-12 px-6">
           <h1 className="px-14 pb-4 text-2xl heading-2">Toxic Silver</h1>
           <p>
-            With its shining aluminum lift and maneuverable mecanum drivetrain, Toxic Silver won us an Excellence in Engineering Award at the Greater Pittsburgh Regional
-            and Rank 8 at the Battle O&apos; Baltimore.
+            With its shining aluminum lift and maneuverable mecanum drivetrain,
+            Toxic Silver won us an Excellence in Engineering Award at the
+            Greater Pittsburgh Regional and Rank 8 at the Battle O&apos;
+            Baltimore.
           </p>
         </div>
       </PatternBackground>
     </div>
-  </>
-),(
+  </>,
   <>
+    <h1 className="heading-2 text-7xl text-center">2020-2021</h1>
     <ParallaxImage background="/image/biohazard/2021-robot-field.webp">
       <SlideButtons></SlideButtons>
     </ParallaxImage>
@@ -47,35 +59,42 @@ const slides = [(
         <div className="py-12 px-6">
           <h1 className="px-14 pb-4 text-2xl heading-2">Robo Fett</h1>
           <p>
-            Armed with a giant scissor-lift climber and an inline shooting mechanism, Robo Fett is one of two Biohazard robots to use pneumatic wheels.
-            Along one side, a large plexiglass panel protects the electronics while proudly displaying the 2020 team logo.
+            Armed with a giant scissor-lift climber and an inline shooting
+            mechanism, Robo Fett is one of two Biohazard robots to use pneumatic
+            wheels. Along one side, a large plexiglass panel protects the
+            electronics while proudly displaying the 2020 team logo.
           </p>
         </div>
       </PatternBackground>
     </div>
-  </>
-),(
+  </>,
   <>
+    <h1 className="heading-2 text-7xl text-center">2022</h1>
     <ParallaxImage background="/image/biohazard/2022-robot-field-3.webp">
       <SlideButtons></SlideButtons>
     </ParallaxImage>
     <div className="border-y-8 border-yellow-400">
       <PatternBackground pattern="circuit">
-      <div className="py-12 px-6">
+        <div className="py-12 px-6">
           <h1 className="px-14 pb-4 text-2xl heading-2">O&apos;Mega</h1>
           <p>
-            One of our heavier robots, O&apos;Mega boasts a giant cannon, a sizeable intake, and two climbing arms. 
-            An adjustable firing mechanism allowed it to shoot into both hoops in the 2022 game, and won us a spot in the finals and an Industrial Design Award.
+            One of our heavier robots, O&apos;Mega boasts a giant cannon, a
+            sizeable intake, and two climbing arms. An adjustable firing
+            mechanism allowed it to shoot into both hoops in the 2022 game, and
+            won us a spot in the finals and an Industrial Design Award.
           </p>
         </div>
       </PatternBackground>
-    </div>  
-  </>
-),(
+    </div>
+  </>,
   <>
+    <h1 className="heading-2 text-7xl text-center">2023</h1>
     <ParallaxImage background="/image/biohazard/2023-robot-field.webp">
-      <Image src={viperLogo} alt="Viper logo" className="relative m-auto inset-y-1/2 w-[200px]">
-      </Image>
+      <Image
+        src={viperLogo}
+        alt="Viper logo"
+        className="relative m-auto inset-y-1/2 w-[200px]"
+      ></Image>
       <SlideButtons></SlideButtons>
     </ParallaxImage>
     <div className="border-y-8 border-yellow-400">
@@ -83,19 +102,25 @@ const slides = [(
         <div className="py-12 px-6">
           <h1 className="px-14 pb-4 text-2xl heading-2">Viper</h1>
           <p>
-            Competing in the 2023 Pittsburgh Regional Competition, Viper won us the Industrial Design Award, and a few curious looks from fellow teams.
-            Its main features are the three foot metal arm and 3d printed pneumatic claw, used to atriculate inflatable cubes.
-            Mecanum wheels allow it to fine-tune its position, and dual cameras gave our drivers lots of visibilty.
+            Competing in the 2023 Pittsburgh Regional Competition, Viper won us
+            the Industrial Design Award, and a few curious looks from fellow
+            teams. Its main features are the three foot metal arm and 3d printed
+            pneumatic claw, used to atriculate inflatable cubes. Mecanum wheels
+            allow it to fine-tune its position, and dual cameras gave our
+            drivers lots of visibilty.
           </p>
         </div>
       </PatternBackground>
     </div>
-  </>
-),(
+  </>,
   <>
+    <h1 className="heading-2 text-7xl text-center">2024</h1>
     <ParallaxImage background="/image/biohazard/2024-robot-field.webp">
-      <Image src={troubleClef} alt="TroubleClef logo" className="relative m-auto inset-y-1/2 w-[400px]">
-      </Image>
+      <Image
+        src={troubleClef}
+        alt="TroubleClef logo"
+        className="relative m-auto inset-y-1/2 w-[400px]"
+      ></Image>
       <SlideButtons></SlideButtons>
     </ParallaxImage>
     <div className="border-y-8 border-yellow-400">
@@ -103,16 +128,20 @@ const slides = [(
         <div className="py-12 px-6">
           <h1 className="px-14 pb-4 text-2xl heading-2">TroubleClef</h1>
           <p>
-            Amid more conventional and exposed designs, TroubleClef sticks out like a sore thumb with its sleek gunmetal sides and concealed electronics.
-            A 3d printed chain carries game pieces from the ground up to the peak of the ramp, and a pair of motors shoot it forward.
-            Coming back from a loss in Pittsburgh, this machine got us 15th place at the Buckeye Valley Regional and helped us win our Imagery Award in the 2023-2024 season.
+            Amid more conventional and exposed designs, TroubleClef sticks out
+            like a sore thumb with its sleek gunmetal sides and concealed
+            electronics. A 3d printed chain carries game pieces from the ground
+            up to the peak of the ramp, and a pair of motors shoot it forward.
+            Coming back from a loss in Pittsburgh, this machine got us 15th
+            place at the Buckeye Valley Regional and helped us win our Imagery
+            Award in the 2023-2024 season.
           </p>
         </div>
       </PatternBackground>
     </div>
-  </>
-),(
+  </>,
   <>
+    <h1 className="heading-2 text-7xl text-center">2025</h1>
     <ParallaxImage>
       <SlideButtons></SlideButtons>
     </ParallaxImage>
@@ -120,20 +149,23 @@ const slides = [(
       <PatternBackground pattern="circuit">
         <div className="py-12 px-6">
           <h1 className="px-14 pb-4 text-2xl heading-2">Coming soon...</h1>
-          <p>
-          </p>
+          <p></p>
         </div>
       </PatternBackground>
     </div>
-  </>
-)]
+  </>,
+];
 
 //Get images from previous years
 export default function Robots() {
   return (
     <>
-      <ManualCarousel slides={slides} noLoop={true} autoHeight={true} startIndex={slides.length - 2}>
-      </ManualCarousel>
+      <ManualCarousel
+        slides={slides}
+        noLoop={true}
+        autoHeight={true}
+        startIndex={slides.length - 2}
+      ></ManualCarousel>
     </>
   );
 }
